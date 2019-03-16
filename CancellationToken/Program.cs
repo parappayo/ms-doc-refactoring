69fd8d5d42e9eced6dad1182c51e0072d473ff1b
Jason Estey <esteyj@yahoo.com>
2019-03-16 12:09:14 -0700

    extract method, simple test

--- CancellationToken/Program.cs
+++ CancellationToken/Program.cs
@@ -9,63 +9,81 @@ namespace CancellationTokenExample
     {
         public static void Main()
         {
-            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            var rnd = new Random();
+            var lockObj = new object();
 
-            Random rnd = new Random();
-            Object lockObj = new Object();
+            var tokenSource = new CancellationTokenSource();
+            var factory = new TaskFactory(tokenSource.Token);
+            var tasks = new List<Task<int[]>>();
 
-            List<Task<int[]>> tasks = new List<Task<int[]>>();
-            TaskFactory factory = new TaskFactory(tokenSource.Token);
+            CreateTasks(tokenSource, rnd, lockObj, tasks, factory);
 
-            for (int taskCtr = 0; taskCtr <= 10; taskCtr++) {
-                int iteration = taskCtr + 1;
-                tasks.Add(factory.StartNew( () => {
-                    int value;
-                    int[] values = new int[10];
-                    for (int ctr = 1; ctr <= 10; ctr++) {
-                        lock (lockObj) {
-                            value = rnd.Next(0, 101);
-                        }
-                        if (value == 0) {
-                            tokenSource.Cancel();
-                            Console.WriteLine("Cancelling at task {0}", iteration);
-                            break;
-                        }
-                        values[ctr-1] = value;
-                    }
-                    return values;
-                }, tokenSource.Token));
-            }
-
-            try {
+            try
+            {
                 Task<double> fTask = factory.ContinueWhenAll(tasks.ToArray(),
-                   (results) => {
-                    Console.WriteLine("Calculating overall mean...");
-                    long sum = 0;
-                    int n = 0;
-                    foreach (var t in results) {
-                        foreach (var r in t.Result) {
-                            sum += r;
-                            n++;
-                        }
-                    }
-                    return sum/(double) n;
-                }, tokenSource.Token);
+                   (results) =>
+                   {
+                       Console.WriteLine("Calculating overall mean...");
+                       long sum = 0;
+                       int n = 0;
+                       foreach (var t in results)
+                       {
+                           foreach (var r in t.Result)
+                           {
+                               sum += r;
+                               n++;
+                           }
+                       }
+                       return sum / (double)n;
+                   }, tokenSource.Token);
                 Console.WriteLine("The mean is {0}.", fTask.Result);
             }
-            catch (AggregateException ae) {
-                foreach (Exception e in ae.InnerExceptions) {
-                    if (e is TaskCanceledException) {
+            catch (AggregateException ae)
+            {
+                foreach (Exception e in ae.InnerExceptions)
+                {
+                    if (e is TaskCanceledException)
+                    {
                         Console.WriteLine("Unable to compute mean: {0}",
-                            ((TaskCanceledException) e).Message);
+                            ((TaskCanceledException)e).Message);
                     }
-                    else {
+                    else
+                    {
                         Console.WriteLine("Exception: " + e.GetType().Name);
                     }
                 }
             }
-            finally {
+            finally
+            {
                 tokenSource.Dispose();
+            }
+        }
+
+        public static void CreateTasks(CancellationTokenSource tokenSource, Random rnd, object lockObj, List<Task<int[]>> tasks, TaskFactory factory)
+        {
+            for (int taskCtr = 0; taskCtr <= 10; taskCtr++)
+            {
+                int iteration = taskCtr + 1;
+                tasks.Add(factory.StartNew(() =>
+                {
+                    int value;
+                    int[] values = new int[10];
+                    for (int ctr = 1; ctr <= 10; ctr++)
+                    {
+                        lock (lockObj)
+                        {
+                            value = rnd.Next(0, 101);
+                        }
+                        if (value == 0)
+                        {
+                            tokenSource.Cancel();
+                            Console.WriteLine("Cancelling at task {0}", iteration);
+                            break;
+                        }
+                        values[ctr - 1] = value;
+                    }
+                    return values;
+                }, tokenSource.Token));
             }
         }
     }
--- CancellationToken/Tests.cs
+++ CancellationToken/Tests.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
 using NUnit.Framework;
 
 namespace CancellationTokenExample
@@ -6,9 +11,17 @@ namespace CancellationTokenExample
     public class Tests
     {
         [Test]
-        public void Failing()
+        public void CreateTasksPopulatesTasks()
         {
-            Assert.IsTrue(false);
+            var rnd = new Random();
+            var lockObj = new object();
+
+            var tokenSource = new CancellationTokenSource();
+            var factory = new TaskFactory(tokenSource.Token);
+            var tasks = new List<Task<int[]>>();
+
+            Program.CreateTasks(tokenSource, rnd, lockObj, tasks, factory);
+            Assert.IsNotEmpty(tasks);
         }
     }
 }
